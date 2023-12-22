68dd1e1be1884cfb80c87ef4427c3f33b293e92a
ViniciusSantos19 <vinicontatosq@gmail.com>
2023-12-22 13:45:54 -0300

    primeiro commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+-- keymaps.lua
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+-- modo normal
+map("n", "<C-s>", ":w<CR>", opts)
+map("n", "<C-q>", ":q!<CR>", opts)
+map("n", "<C-a>", "<Esc>ggVG", opts)
+
+-- Modo Visual
+map("x", "<C-a>", "ggVG", opts)
+
+-- Modo de Inserção
+map("i", "<C-a>", "<Esc>ggVG", opts)
